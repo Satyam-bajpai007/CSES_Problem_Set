b8a9fe729348181cecae204b036a3332dc38c78d
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-09-20 20:14:43 +0530

    added

--- Weird_Algorithm.py
+++ Weird_Algorithm.py
@@ -9,8 +9,8 @@ lst = [n]
 while n!=1:
     if n%2 == 0:
         n = n/2
-        lst.append(n)
+        lst.append(int(n))
     else:
         n = n*3 + 1
-        lst.append(n)
-st=" ".join(lst)
+        lst.append(int(n))
+print(*lst)
