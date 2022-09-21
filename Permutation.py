cfa670ad96a4fad2ef8593f8bc6877125d364f16
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-09-21 07:48:29 +0530

    added

---
+++ Permutation.py
@@ -0,0 +1,6 @@
+# A permutation of integers 1,2,…,n is called beautiful if there
+# are no adjacent elements whose difference is 1.
+
+# Given n, construct a beautiful permutation if such a permutation
+# exists.
+
