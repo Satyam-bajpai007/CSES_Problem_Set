9a39745b000f7d0a23858e5d82589cc54de226cd
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-09-23 23:38:47 +0530

    added

--- Two_knghts.py
+++ Two_knghts.py
@@ -2,3 +2,10 @@
 # knights can be placed on a k×k chessboard so that they do not
 # attack each other.
 
+n = int(input())
+org = 0
+flt = 0
+for i in range(1,n+1):
+    org = ((i**2)*(i**2-1))/2
+    flt = 4*(i-1)*(i-2)
+    print(int(org-flt))
