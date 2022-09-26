3512354abfe0d8c563eaa8401178902e0c9ac52e
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-09-26 19:47:51 +0530

    added

--- Palindrome_Reorder.py
+++ Palindrome_Reorder.py
@@ -3,3 +3,37 @@
 # and backwards).
 
 
+n = input().upper()
+dic={}
+cnt=0
+st=""
+left=""
+
+for i in n:
+    if i in dic:
+        dic[i]+=1 
+    else:
+        dic[i]=1
+        
+for i in dic.values():
+    if i%2!=0:
+        cnt+=1
+        
+print(dic)
+
+if cnt > 1:
+    print("NO SOLUTION")
+else:
+    if cnt == 0:
+        for i in dic:
+            st+=i
+        st += st[::-1] 
+        print(st)
+    else:
+        for i in dic:
+            if dic[i]%2==0:
+                st+=i
+            else:
+                left=i
+        st = st + left*dic[left] + st[::-1]
+        print(st)
