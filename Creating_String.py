5c9420b403bb1a3edcbdb4b65031e05211317a4c
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-10-07 11:19:05 +0530

    added

--- Creating_String.py
+++ Creating_String.py
@@ -2,4 +2,9 @@
 # that can be created using its characters.
 
 
+from itertools import permutations
 n = input()
+lst = sorted(["".join(i) for i in list(set(permutations(n)))])
+# print(lst)
+for i in lst:
+    print(i)
