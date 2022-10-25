82fd80343f131151fcc7577ca368d025a8313292
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-10-25 11:56:48 +0530

    added

---
+++ Chessboard_and_Queens.py
@@ -0,0 +1,10 @@
+# Your task is to place eight queens on a chessboard so that no
+# two queens are attacking each other. As an additional challenge,
+# each square is either free or reserved, and you can only place
+# queens on the free squares. However, the reserved squares do not
+# prevent queens from attacking each other.
+
+# How many possible ways are there to place the queens?
+
+
+89
