ce74c2122d21ca6ed495f9da180090f02bcba3ab
Satyam Bajpai <66032266+Satyam-bajpai007@users.noreply.github.com>
2022-10-25 12:22:19 +0530

    added

---
+++ Digit_Queries.py
@@ -0,0 +1,9 @@
+# Consider an infinite string that consists of all positive integers
+# in increasing order:
+
+# 12345678910111213141516171819202122232425...
+
+# Your task is to process q queries of the form: what is the digit
+# at position k in the string?
+
+
